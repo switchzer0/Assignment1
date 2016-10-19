3f1b38d24519851f026d78890532afbdcb1d9248
switchzer0 <adam.spar@gmail.com>
2016-10-19 18:35:13 -0400

    revised function

--- corr.R
+++ corr.R
@@ -2,22 +2,12 @@ corr <- function(directory, threshold = 0) {
         #get the list of files; identify length
         file_list <-list.files(directory, full.names = TRUE)
         file_quantity <- length(file_list)
-        #create data.frame
+        #create numeric vector
+        cors <- as.numeric()
         dat <- data.frame()
-        #loop through files selecting only those that meet the correlation threshold and adding to data frame
-                #take file and count complete cases
-                for (i in file_quantity) {
-                        cases_count <- sum(complete.cases(read.csv(file_list[i])))
-                        #ask "does this meet the threshold?"
-                        if(cases_count >= threshold) dat <- rbind(dat, read.csv(file_list[i]))
-                }
-        #return numeric vector correlating sulfate and nitrate
-        cor()
-                #subset data.frame to get the sulfate column
-                #subset data.frame to get the nitrate column
-        
-        
-        #ask if nrow > 0; if yes, print it; if no return numeric vector length 0
-        if(nrow(dat > 0)) print(vector(mode = "numeric"))
+        #loop through files, IDing those that meet the threshold, running cor() on those ones only, and adding to vector
+        for (i in file_quantity) {
+                if(as.integer(subset(complete(directory,i),select = nobs)) >= threshold) dat <- read.csv(file_list[i])
+                cors <- as.vector(rbind(cors, cor(dat[,2], dat[,3], use = "na.or.complete")))
         }
 }
