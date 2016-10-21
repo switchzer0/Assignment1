cbaa7c08fe152dfceb2e31d17142a9623b93be34
switchzer0 <adam.spar@gmail.com>
2016-10-20 22:50:32 -0400

    edits finished, committing completed assignment

--- complete.R
+++ complete.R
@@ -2,12 +2,12 @@ complete <- function(directory, id = 1:332) {
         #get list of files
         file_list <-list.files(directory, full.names = TRUE)
         
-        #loop through files, adding computed data from ones with complete cases to dataframefor (i in id) {
+        #loop through files, adding computed data from ones with complete cases to data.frame for (i in id) {
         dat <- data.frame()
         for (i in id) {
                 cases_count <- sum(complete.cases(read.csv(file_list[i])))
                 dat <- rbind(dat, c(i, cases_count))
-                }
+        }
 
         #specify colnames
         colnames(dat) <- c("id","nobs")
--- corr.R
+++ corr.R
@@ -2,12 +2,17 @@ corr <- function(directory, threshold = 0) {
         #get the list of files; identify length
         file_list <-list.files(directory, full.names = TRUE)
         file_quantity <- length(file_list)
-        #create numeric vector
+        
+        #create numeric vector and data.frame
         cors <- as.numeric()
         dat <- data.frame()
-        #loop through files, IDing those that meet the threshold, running cor() on those ones only, and adding to vector
-        for (i in file_quantity) {
-                if(as.integer(subset(complete(directory,i),select = nobs)) >= threshold) dat <- read.csv(file_list[i])
-                cors <- as.vector(rbind(cors, cor(dat[,2], dat[,3], use = "na.or.complete")))
+        #loop through files, running cor() on those those greater than threshold
+        for (i in 1:file_quantity) {
+                dat <- read.csv(file_list[i])
+                if(sum(complete.cases(dat)) > threshold) {
+                        cors <- rbind(cors, cor(dat[,2], dat[,3], use = "na.or.complete"))
+                }
         }
+                #coerce cors to vector
+                as.vector(cors)
 }
